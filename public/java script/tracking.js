1b233c81adc1e4036818850f846b2a755d8adf7f
Rohit <rs8987319@gmail.com>
2022-02-06 10:30:52 +0530

    adding tracing script

--- public/java script/tracking.js
+++ public/java script/tracking.js
@@ -1,13 +1,10 @@
- (function(v, i, s, a, t) {
-     v[t] = v[t] || function() {
-         (v[t].v = v[t].v || []).push(arguments)
-     };
-     if (!v._visaSettings) { v._visaSettings = {} }
-     v._visaSettings[a] = { v: '1.0', s: a, a: '1', t: t };
-     var b = i.getElementsByTagName('body')[0];
-     var p = i.createElement('script');
-     p.defer = 1;
-     p.async = 1;
-     p.src = s + '?s=' + a;
-     b.appendChild(p)
- })(window, document, '//app-worker.visitor-analytics.io/main.js', '2081c5a3-8693-11ec-b589-901b0edac50a', 'va')
+(function(c, l, a, r, i, t, y) {
+    c[a] = c[a] || function() {
+        (c[a].q = c[a].q || []).push(arguments)
+    };
+    t = l.createElement(r);
+    t.async = 1;
+    t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0];
+    y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "ai9j44vr1q");
